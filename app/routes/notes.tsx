8f8710177941a8509df164db46ef337b2b822fe4
Vishal Sharma <vishal.sharma@guccidigital.com>
2023-01-29 23:19:38 +0530

    form submission

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,14 +1,48 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { ActionArgs, LinksFunction, LoaderArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import NoteList, { links as noteListLinks } from "~/components/NoteList";
+import { getStoredNotes, Note, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
+  const notes = useLoaderData<typeof loader>();
+
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes} />
     </main>
   );
 }
 
+export async function loader() {
+  const notes = await getStoredNotes();
+
+  return json(notes);
+}
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+
+  const title = formData.get('title');
+  const content = formData.get('title');
+
+  if (title && content) {
+    const note: Note = {
+      id: new Date().toISOString(),
+      title,
+      content,
+    };
+
+    const existingNotes = await getStoredNotes();
+    note.id = new Date().toISOString();
+    await storeNotes(existingNotes.concat(note));
+    return redirect('/notes');
+  }
+}
+
 export const links: LinksFunction = () => {
-  return [...newNoteLinks()];
+  return [...newNoteLinks(), ...noteListLinks()];
 }
